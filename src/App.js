8196113205eb70eb964d98356bb39f6e4a800483
Ainlautaro <ainlautaro7@gmail.com>
2021-05-16 14:05:18 -0300

    Update App.js
    Se incluyerron los nuevos componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import Actionbar from './components/Actionbar';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Actionbar/>
       <Sidebar/>
+      <Home/>
     </div>
   );
 }
