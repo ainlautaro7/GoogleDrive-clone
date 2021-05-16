1a368a2871c9c9717a58abd95f880480c5582a93
Ainlautaro <ainlautaro7@gmail.com>
2021-05-16 14:04:28 -0300

    Create Actionbar.js
    Se creo el componente.

---
+++ src/components/Actionbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Actionbar extends React.Component{
+    render(){
+        return(
+            <div className="Actionbar">
+                <div className="Actionbar-item">
+                    <button className="Actionbar-action-button">Mi Unidad</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Actionbar;
