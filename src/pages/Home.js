6f63f6f958de9444c5c76b3d30ad66d5f544a0b6
Ainlautaro <ainlautaro7@gmail.com>
2021-05-16 14:05:04 -0300

    Create Home.js
    Se creo el componente

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div className="Home">
+                <h5>Acceso rápido</h5>
+            </div>
+        );
+    }
+}
+
+export default Home;
